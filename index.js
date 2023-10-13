678efc1b71a10858e490d2e9ad67a5c423a467cb
64160088 <64160088@go.buu.ac.th>
2023-10-13 13:34:38 +0700

    Can_Upload_Pic

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const ejs = require('ejs');
 const port = 3000;
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer({ dest: 'public/uploads/' });
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.json());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
@@ -26,7 +28,7 @@ connection.connect((error) => {
 });
 
 // Home page
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
     // Fetch products from the database
     connection.query('SELECT * FROM products', (error, results) => {
         if (error) {
@@ -38,6 +40,31 @@ app.get('/', async(req, res) => {
     });
 });
 
+app.get('/products', (req, res) => {
+    res.render('pages/products');
+});
+
+app.post('/products', upload.single('image'), (req, res) => {
+    let name = req.body.name;
+    let description = req.body.description;
+    let price = req.body.price;
+    let sale_price = req.body.sale_price;
+    let quantity = req.body.quantity;
+    let image = req.file.filename;
+    let category = req.body.category;
+    let type = req.body.type;
+    let rating = req.body.rating;
+    let sql = `INSERT INTO products (name, description, price, sale_price, quantity, image, category, type , rating) VALUES ('${name}', '${description}', '${price}', '${sale_price}', '${quantity}', '${image}', '${category}', '${type}' ,'${rating}')`;
+    connection.query(sql, (error, results) => {
+        if (error) {
+            console.error('Error inserting product: ', error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.redirect('/products');
+        }
+    });
+});
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+// name	description	price	sale_price	quantity	image	category	type
